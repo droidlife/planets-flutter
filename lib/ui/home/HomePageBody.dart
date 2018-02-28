50d7ee623c2556830fb4bf0dd88329ec6fab6f17
ankur <ankurj630@gmail.com>
2018-02-28 17:03:26 +0530

    scroll view added

--- lib/ui/home/HomePageBody.dart
+++ lib/ui/home/HomePageBody.dart
@@ -5,13 +5,25 @@ import 'package:planets/model/planets.dart';
 class HomePageBody extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return new Column(
-      children: <Widget>[
-        new PlanetRow(planets[0]),
-        new PlanetRow(planets[1]),
-        new PlanetRow(planets[2]),
-      ],
+    return new Expanded(
+      child: new Container(
+        color: new Color(0xFF736AB7),
+        child: new CustomScrollView(
+          scrollDirection: Axis.vertical,
+          slivers: <Widget>[
+            new SliverPadding(
+              padding: const EdgeInsets.symmetric(vertical: 24.0),
+              sliver: new SliverFixedExtentList(
+                itemExtent: 154.0,
+                delegate: new SliverChildBuilderDelegate(
+                  (context, index) => new PlanetRow(planets[index]),
+                  childCount: planets.length
+                )
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
-
 }
